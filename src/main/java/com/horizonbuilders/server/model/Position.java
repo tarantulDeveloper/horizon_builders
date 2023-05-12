2a7f9ee81b5f0a20c093e4780510061dd12a3084
nzima17 <92359825+nzima17@users.noreply.github.com>
2023-05-12 13:33:49 +0600

    added position model

---
+++ src/main/java/com/horizonbuilders/server/model/Position.java
@@ -0,0 +1,17 @@
+package com.horizonbuilders.server.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Position extends DefaultModel{
+    String name;
+    double salary;
+}
