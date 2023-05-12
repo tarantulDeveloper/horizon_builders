b64f9a2abe3f9f856e92dcada833214d88d3d2ca
nzima17 <92359825+nzima17@users.noreply.github.com>
2023-05-12 13:47:08 +0600

    added user service

--- src/main/java/com/horizonbuilders/server/service/UserService.java
+++ src/main/java/com/horizonbuilders/server/service/UserService.java
@@ -1,4 +1,11 @@
 package com.horizonbuilders.server.service;
 
+import com.horizonbuilders.server.dto.request.UserRequest;
+import com.horizonbuilders.server.dto.response.UserResponse;
+import com.horizonbuilders.server.model.User;
+
 public interface UserService {
+
+    UserResponse addNewUser(int positionId,String username,String password);
+
 }
