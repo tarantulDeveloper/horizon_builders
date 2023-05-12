6c343a30b1d8bde4e21147315b636ff89ddb8c18
nzima17 <92359825+nzima17@users.noreply.github.com>
2023-05-12 13:44:48 +0600

    added some fields

--- src/main/java/com/horizonbuilders/server/model/inventory/Product.java
+++ src/main/java/com/horizonbuilders/server/model/inventory/Product.java
@@ -2,15 +2,19 @@ package com.horizonbuilders.server.model.inventory;
 
 import com.horizonbuilders.server.model.DefaultModel;
 import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product extends DefaultModel {
     String name;
+    int quantity;
+    String imgUrl;
+    double price;
 }
