e65c37651ec85df36f70136fbf0e81739084da3d
nzima17 <92359825+nzima17@users.noreply.github.com>
2023-05-12 13:44:24 +0600

    deleted unneeded things

--- src/main/java/com/horizonbuilders/server/controller/UserController.java
+++ src/main/java/com/horizonbuilders/server/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.horizonbuilders.server.controller;
 
+import com.horizonbuilders.server.dto.request.UserRequest;
+import com.horizonbuilders.server.dto.response.UserResponse;
+import com.horizonbuilders.server.service.impl.UserServiceImpl;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserController {
+    @Autowired
+    final UserServiceImpl userService;
 
-
+    @PostMapping
+    public UserResponse addUser(@RequestParam("id") int positionId,
+                                @RequestParam("username")String username,
+                                @RequestParam("password")String password){
+        return userService.addNewUser(positionId,username,password);
+    }
 }
