a541f49c40ff03ccef5f3e41b296fc4a312dc094
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-19 20:28:02 +0600

    projection global type

--- src/main/java/com/horizonbuilders/server/repository/GlobalTypeRepository.java
+++ src/main/java/com/horizonbuilders/server/repository/GlobalTypeRepository.java
@@ -1,10 +1,15 @@
 package com.horizonbuilders.server.repository;
 
 import com.horizonbuilders.server.model.inventory.GlobalType;
+import com.horizonbuilders.server.repository.projections.GlobalTypeListView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GlobalTypeRepository extends JpaRepository<GlobalType, Integer> {
     boolean existsByName(String name);
+
+    Page<GlobalTypeListView> findAllProjectedBy(Pageable pageable);
 }
