f17f382c0fadce0d4c4015bed1de41c81c69be8d
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-20 00:51:35 +0600

    removed lock icons from methods

--- src/main/java/com/horizonbuilders/server/controller/TestController.java
+++ src/main/java/com/horizonbuilders/server/controller/TestController.java
@@ -2,11 +2,13 @@ package com.horizonbuilders.server.controller;
 
 import com.horizonbuilders.server.repository.GlobalTypeRepository;
 import com.horizonbuilders.server.repository.projections.GlobalTypeListView;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +20,12 @@ public class TestController {
     @Autowired
     GlobalTypeRepository globalTypeRepository;
     @GetMapping("/test")
+    @SecurityRequirements
     public List<String> getSampleString() {
         return List.of("Bekzhan", "Nazima", "Zhoomart", "Aigul");
     }
     @GetMapping("/test/global-types")
+    @SecurityRequirements
     Page<GlobalTypeListView> getAllGlobalsListView(
             @RequestParam(defaultValue = "0") int pageNo,
             @RequestParam(defaultValue = "3") int pageSize,
