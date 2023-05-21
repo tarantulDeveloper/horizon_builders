70c19720cc500de5188e1393350038e62c72b5e9
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-21 15:13:25 +0600

    order entity

--- src/main/java/com/horizonbuilders/server/model/Order.java
+++ src/main/java/com/horizonbuilders/server/model/Order.java
@@ -1,21 +1,17 @@
 package com.horizonbuilders.server.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Entity
+@Table(name = "orders")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Request extends DefaultModel{
-    String customerName;
-    String phoneNumber;
-    @Column(columnDefinition = "TEXT")
-    String message;
+public class Order extends DefaultModel {
 }
